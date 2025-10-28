27b235155dc6e1d249ea843c6ae1035add45d2e4
phgnm <phuongnam710648@gmail.com>
2025-10-28 10:11:42 +0700

    feat: init project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-pink-500 to-purple-600 text-white text-3xl font-bold">
+      Tailwind v4 + Vite + React 🔥
+    </div>
+  )
+}
+
+export default App
